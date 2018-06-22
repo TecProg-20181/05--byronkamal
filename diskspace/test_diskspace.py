c8e6453046098698a87dc034b42b327c7ecb9137
byronkamal <byronkamal@hotmail.com>
2018-06-21 23:23:54 -0300

    Testando a função test_show_space_list

--- diskspace/test_diskspace.py
+++ diskspace/test_diskspace.py
@@ -6,13 +6,16 @@ class TestDiskspace(unittest.TestCase):
 
  def test_subprocess_check_output(self):
     command = 'ls'
-    check = subprocess_check_output(command)
-    self.assertEqual(subprocess_check_output(command), check)
+    method = subprocess_check_output(command)
+    self.assertEqual(subprocess_check_output(command), method)
 
  def test_bytes_to_readable(self):
     command = bytes_to_readable(512)
     self.assertEqual(command, '256.00Kb')
     self.assertNotEqual(command, '512.00Kb')
+
+ def test_show_space_list(self):
+    self.assertIsNone(show_space_list(directory='.', depth=-1, order=True))
 
 
 if __name__ == '__main__':
